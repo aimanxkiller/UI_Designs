8e3e240ce9f4adf8804870e84aa4e978f89527a2
aimanxkiller <aimanxkiller@gmail.com>
2023-02-20 16:14:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/uidesigns/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.uidesigns
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.example.uidesigns.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setting binding for main
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bar = binding.progressBar
+        bar.max = 1000
+        val currentProgress = 1000
+
+
+        ObjectAnimator.ofInt(bar,"progress",currentProgress)
+            .setDuration(3000).start()
+
+        lifecycleScope.launch(Dispatchers.Main){
+
+            delay(5000)
+
+            val intent = Intent(this@MainActivity,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+}
