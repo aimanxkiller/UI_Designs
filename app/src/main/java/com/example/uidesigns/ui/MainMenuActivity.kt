f74df61287e964a4a2d6fc0ee4cc391424a36757
aimanxkiller <aimanxkiller@gmail.com>
2023-02-22 16:42:56 +0800

    Adjusted paddings, width and height to wrap content/match parent to allow better alignment with variable size devices.
    -Adjusted paddings
    
    -Adding bottomNav and fragments for multiple activities/views
    -Added fragments
    TODO(
    - Top view
    - Item View for activities that changes colors by (Daily,Weekly,Monthly)
    
    )

--- app/src/main/java/com/example/uidesigns/ui/MainMenuActivity.kt
+++ app/src/main/java/com/example/uidesigns/ui/MainMenuActivity.kt
@@ -2,15 +2,42 @@ package com.example.uidesigns.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.uidesigns.R
+import com.example.uidesigns.databinding.ActivityMainMenuBinding
+import com.example.uidesigns.fragments.*
 
 class MainMenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_menu)
+        binding = ActivityMainMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFrags(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFrags(Home())
+                R.id.calendar -> replaceFrags(Calendar())
+                R.id.history -> replaceFrags(History())
+                R.id.profile -> replaceFrags(Profile())
+
+                else ->{
+                }
+            }
+            true
+        }
+
 
     }
 
+    private fun replaceFrags(fragment:Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+    }
 }
