1770d4312fd158f28e145d76480a50354dd664bc
aimanxkiller <aimanxkiller@gmail.com>
2023-02-28 14:59:47 +0800

    -Added parse to calendar selection
    TODO(
    -Correction for Set New Schedule on Distributor selection (Use another custom dialog)
    -Calendar fragment (Insert adapter)
    -History fragment
    )

--- app/src/main/java/com/example/uidesigns/fragments/History.kt
+++ app/src/main/java/com/example/uidesigns/fragments/History.kt
@@ -5,22 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.uidesigns.R
+import com.example.uidesigns.model.TaskModel
+import com.example.uidesigns.model.TaskList
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [History.newInstance] factory method to
- * create an instance of this fragment.
- */
 class History : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var data:ArrayList<TaskModel>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,26 +41,26 @@ class History : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment History.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            History().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        val view = inflater.inflate(R.layout.fragment_history, container, false)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_history)
+
+        data = arrayListOf(
+            TaskModel("Task 1",3),
+            TaskModel("Task 2",2),
+            TaskModel("Task 3",1),
+            TaskModel("Task 10",1),
+            TaskModel("Task 11",1),
+        )
+
+        lifecycleScope.launch(Dispatchers.IO){
+            recyclerView.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                //todo insert new adapter for history
+//                recyclerViewAdapter = BaseAdapter()
             }
+        }
+
+        return view
     }
 }
