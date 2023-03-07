51c51035c72d07af6d5f2085f93e7240958a8bc1
aimanxkiller <aimanxkiller@gmail.com>
2023-03-07 14:29:54 +0800

    - Completed fill up inspection activity - Completed all activity and fragments with placeholders
    - Fixed dark mode causing cardView changing color
    - Added upload picture from gallery and get the name etc
    -

--- app/src/main/java/com/example/uidesigns/fragments/Calendar.kt
+++ app/src/main/java/com/example/uidesigns/fragments/Calendar.kt
@@ -1,6 +1,7 @@
 package com.example.uidesigns.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
