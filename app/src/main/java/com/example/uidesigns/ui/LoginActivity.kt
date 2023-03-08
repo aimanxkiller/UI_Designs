06967277395fd71bf4e7d60598db41b0d5be5456
aimanxkiller <aimanxkiller@gmail.com>
2023-03-08 12:30:22 +0800

    - Added capture img from camera - Added request permission upon login

--- app/src/main/java/com/example/uidesigns/ui/LoginActivity.kt
+++ app/src/main/java/com/example/uidesigns/ui/LoginActivity.kt
@@ -1,13 +1,21 @@
 package com.example.uidesigns.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.uidesigns.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityLoginBinding
+
+    companion object{
+        private const val PERMISSIONS_REQUEST_CODE = 10
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +24,58 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonSignIn.setOnClickListener {
+            requestPermissions()
+        }
+    }
+
+    private fun requestPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ),
+                PERMISSIONS_REQUEST_CODE
+            )
+        } else {
+            // Permissions granted
             val intent = Intent(this,MainMenuActivity::class.java)
             startActivity(intent)
             finish()
         }
     }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                // Permissions granted
+                val intent = Intent(this,MainMenuActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                // Permissions denied
+            }
+        }
+    }
+
 
 }
