908dee2bf96c2146409cbb5ead8a750f34f85cad
aimanxkiller <aimanxkiller@gmail.com>
2023-03-15 14:07:56 +0800

    DONE ( - Custom calendar view with grey date, highlight date on selected )
    TODO (
    - Custom bottom navigation bar later on
    )

--- app/src/main/java/com/example/uidesigns/fragments/Calendar.kt
+++ app/src/main/java/com/example/uidesigns/fragments/Calendar.kt
@@ -1,12 +1,12 @@
 package com.example.uidesigns.fragments
 
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CalendarView
 import android.widget.TextView
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,9 +14,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.uidesigns.R
 import com.example.uidesigns.adapter.RecyclerViewAdapterCalendar
 import com.example.uidesigns.databinding.CalendarDayLayoutBinding
-import com.example.uidesigns.databinding.FragmentCalendarBinding
 import com.example.uidesigns.model.TaskModel
 import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import com.kizitonwose.calendar.view.MonthDayBinder
 import com.kizitonwose.calendar.view.ViewContainer
@@ -27,6 +28,8 @@ import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.time.temporal.ChronoUnit
 import java.time.temporal.TemporalField
 import java.time.temporal.WeekFields
 import java.util.*
@@ -44,16 +47,13 @@ class Calendar : Fragment() {
 
     private lateinit var data:ArrayList<TaskModel>
     private lateinit var recyclerViewAdapter: RecyclerViewAdapterCalendar
-    private lateinit var binding:FragmentCalendarBinding
-
     private var selectedDate: LocalDate? = null
+
     private val today = LocalDate.now()
     private val fieldUS: TemporalField = WeekFields.of(Locale.US).dayOfWeek()
     private val startDay = today.with(fieldUS, 1).minusDays(1)
     private val lastDay = today.with(fieldUS, 7).plusDays(1)
     private val titleFormatter = DateTimeFormatter.ofPattern("MMMM yyyy", Locale.US)
-    private val serverFormatter by lazy { DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.US) }
-    private val noInspectionFormatter by lazy { DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.US) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,11 +67,11 @@ class Calendar : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        val view = inflater.inflate(R.layout.fragment_calendar, container, false)
 
-        val recyclerView = binding.recyclerCalendar
-        val curMonth = binding.textCurrentMonth
-        val calendarView = binding.calendarView
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_calendar)
+        val curMonth = view.findViewById<TextView>(R.id.textCurrentMonth)
+        val calendarView = view.findViewById<com.kizitonwose.calendar.view.CalendarView>(R.id.calendarView)
 
         setCurrentMonth(curMonth)
 
@@ -92,12 +92,12 @@ class Calendar : Fragment() {
             }
         }
 
+        selectedDate = LocalDate.now()
 
         //start
         val minusMonth = if (startDay.monthValue == YearMonth.now().monthValue) 0 else 1
         val plusMonth = if (lastDay.monthValue == YearMonth.now().monthValue) 0 else 1
-
-        val daysOfWeek = daysOfWeekFromLocale()
+        val daysOfWeek = daysOfWeek(firstDayOfWeek = DayOfWeek.SUNDAY)
 
         val currentMonth = YearMonth.now()
         calendarView.apply {
@@ -106,30 +106,93 @@ class Calendar : Fragment() {
         }
 
         class DayViewContainer(view: View) : ViewContainer(view) {
+            lateinit var day: CalendarDay // Will be set when this container is bound.
             val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
-        }
 
-
-
-        calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
-            // Called only when a new container is needed.
-            override fun create(view: View) = DayViewContainer(view)
-
-            // Called every time we need to reuse a container.
-            override fun bind(container: DayViewContainer, data: CalendarDay) {
-                container.textView.text = data.date.dayOfMonth.toString()
+            init {
+                view.setOnClickListener {
+                    // Check the day position as we do not want to select in or out dates.
+                    if (day.position == DayPosition.MonthDate) {
+                        // Keep a reference to any previous selection
+                        // in case we overwrite it and need to reload it.
+                        val currentSelection = selectedDate
+                        if (currentSelection == day.date) {
+                            // If the user clicks the same date, clear selection.
+                            selectedDate = null
+                            // Reload this date so the dayBinder is called
+                            // and we can REMOVE the selection background.
+                            calendarView.notifyDateChanged(currentSelection)
+                        } else {
+                            selectedDate = day.date
+                            // Reload the newly selected date so the dayBinder is
+                            // called and we can ADD the selection background.
+                            calendarView.notifyDateChanged(day.date)
+                            if (currentSelection != null) {
+                                // We need to also reload the previously selected
+                                // date so we can REMOVE the selection background.
+                                calendarView.notifyDateChanged(currentSelection)
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        
-        val startMonth = currentMonth.minusMonths(100)  // Adjust as needed
-        val endMonth = currentMonth.plusMonths(100)  // Adjust as needed
+        //When scroll change title set month
+        calendarView.monthScrollListener = {
+            curMonth.text = titleFormatter.format(it.yearMonth)
+        }
+
+        //setting months limit
+        val startMonth = currentMonth.minusMonths(2)  // Adjust as needed
+        val endMonth = currentMonth.plusMonths(5)  // Adjust as needed
         val firstDayOfWeek = firstDayOfWeekFromLocale() // Available from the library
         calendarView.setup(startMonth, endMonth, firstDayOfWeek)
         calendarView.scrollToMonth(currentMonth)
 
+        //Setting title to short type Mon,Tue, etc...
+        val titlesContainer = view.findViewById<ViewGroup>(R.id.titlesContainer)
+        titlesContainer.children
+            .map { it as TextView }
+            .forEachIndexed { index, textView ->
+                val dayOfWeek = daysOfWeek[index]
+                val title = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())
+                textView.text = title
+            }
+
+        //Color changing date for month
+        calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
+            override fun create(view: View) = DayViewContainer(view)
+            override fun bind(container: DayViewContainer, data: CalendarDay) {
+                container.day = data
+                val day = data
+                container.textView.text = data.date.dayOfMonth.toString()
+
+                //Changing the text to number date
+                if (data.position == DayPosition.MonthDate) {
+                    //if day selected change to highlight
+                    if(day.date == selectedDate){
+                        container.textView.setTextColor(Color.WHITE)
+                        container.textView.setBackgroundResource(R.drawable.example_3_today_bg)
+                    //if day is not selected no background
+                    }else{
+                        container.textView.background = null
+                        //black only for current week centered on current date
+                        if(data.date == LocalDate.now() || data.date.until(LocalDate.now(), ChronoUnit.DAYS) in -2..4){
+                            container.textView.setTextColor(Color.BLACK)
+                        }else{
+                            //other days greyed
+                            container.textView.setTextColor(Color.GRAY)
+                        }
+                    }
+                } else {
+                    container.textView.visibility = View.INVISIBLE
+                }
+            }
+        }
+
         // Inflate the layout for this fragment
-        return binding.root
+        return view
     }
 
     private fun setCurrentMonth(curMonth: TextView) {
@@ -141,20 +204,7 @@ class Calendar : Fragment() {
         val inputDateFormat = SimpleDateFormat("M yyyy", Locale.US)
         val outputDateFormat = SimpleDateFormat("MMMM yyyy",Locale.US)
 
-        curMonth.text = outputDateFormat.format(inputDateFormat.parse(x))
-    }
-
-    fun daysOfWeekFromLocale(): Array<DayOfWeek> {
-        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
-        var daysOfWeek = DayOfWeek.values()
-        // Order `daysOfWeek` array so that firstDayOfWeek is at index 0.
-        // Only necessary if firstDayOfWeek != DayOfWeek.MONDAY which has ordinal 0.
-        if (firstDayOfWeek != DayOfWeek.MONDAY) {
-            val rhs = daysOfWeek.sliceArray(firstDayOfWeek.ordinal..daysOfWeek.indices.last)
-            val lhs = daysOfWeek.sliceArray(0 until firstDayOfWeek.ordinal)
-            daysOfWeek = rhs + lhs
-        }
-        return daysOfWeek
+        curMonth.text = outputDateFormat.format(inputDateFormat.parse(x) as Date)
     }
 
 }
